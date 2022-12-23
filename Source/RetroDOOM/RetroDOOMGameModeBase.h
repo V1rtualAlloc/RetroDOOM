11dc55a2f217723e922d9ba458b27d93675ae28f
V1rtualAlloc <kocovic.vladimir@gmail.com>
2022-12-24 00:25:52 +0100

    textures, materials, and basic game mode, player, and level settings

---
+++ Source/RetroDOOM/RetroDOOMGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RetroDOOMGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RETRODOOM_API ARetroDOOMGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
